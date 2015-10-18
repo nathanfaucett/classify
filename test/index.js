2c6557d71f438ecc15cb59ed679b281d023c198e
nathanfaucett <nathanfaucett@gmail.com>
2015-10-18 16:42:11 -0500

    updates

---
+++ test/index.js
@@ -0,0 +1,19 @@
+var tape = require("tape"),
+    inflections = require("inflections"),
+    classify = require("..");
+
+
+tape("classify(string : String[, locale : String])", function(assert) {
+    var en = inflections("en");
+
+    en.singular(/s$/i, "");
+    en.singular(/(ch|sh|ss|[sxz])es$/i, "$1");
+    en.singular(/([^aeiouy])ies$/i, "$1y");
+
+    assert.equal(classify("boxes"), "Box");
+    assert.equal(classify("skies"), "Sky");
+    assert.equal(classify("bike_riders"), "BikeRider");
+    assert.equal(classify("mega-big-sales"), "MegaBigSale");
+
+    assert.end();
+});
